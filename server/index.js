5d239dcacc2775699a56e61b1194b3b16c78a021
tiatiahwang <tiahwang44@gmail.com>
2023-02-17 20:33:11 +0900

    서버 라우터 설정

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const cors = require('cors');
+const router = require('./router');
 
-const http = require('http').createServer(app);
-http.listen(8080, function () {
-  console.log('8080 오바');
+const CLIENT_URL = 'http://localhost:3000';
+const PORT = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }),
+);
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT} port`);
 });
